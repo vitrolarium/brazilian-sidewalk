f70ee7e8686ec15ab430964db1d86692aafe95f5
vitrolarium <107374096+vitrolarium@users.noreply.github.com>
2023-05-06 00:55:18 -0300

    infinite scrolling done
    suck my dick

--- Objects/Scenes/sidewalk_manager.gd
+++ Objects/Scenes/sidewalk_manager.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @export_category("Sidewalk Settings")
 @export var sidewalk_speed : int
+@export var alt_sidewalk_chance : float
 
 @export_category("Sidewalk Objects")
 @export var main_sidewalk : PackedScene
@@ -24,16 +25,24 @@ func _physics_process(delta: float) -> void:
 	if is_enabled:
 		move_sidewalks(sidewalk_speed * delta)
 
-
 func move_sidewalks(speed : float):
 	for sidewalk in current_sidewalks:
 		sidewalk.position.y += speed
-		if speed > 0 and sidewalk.position.y > _viewport_size.y:
-			sidewalk.position.y -= _viewport_size.y * 3
-
-
-func _on_button_pressed() -> void:
-	if is_enabled:
-		sidewalk_speed = int($TextEdit.text)
-	else:
-		is_enabled = true
+		
+		if sidewalk.position.y > _viewport_size.y:
+			if randf() < alt_sidewalk_chance:
+				var new_sidewalk = alt_sidewalk.pick_random().instantiate() as TileMap
+				new_sidewalk.position.y = sidewalk.position.y - _viewport_size.y * 3
+				current_sidewalks.push_back(new_sidewalk)
+				add_child(new_sidewalk)
+				
+				current_sidewalks.erase(sidewalk)
+				sidewalk.queue_free()
+			else:
+				var new_sidewalk = main_sidewalk.instantiate() as TileMap
+				new_sidewalk.position.y = sidewalk.position.y - _viewport_size.y * 3
+				current_sidewalks.push_back(new_sidewalk)
+				add_child(new_sidewalk)
+				
+				current_sidewalks.erase(sidewalk)
+				sidewalk.queue_free()
