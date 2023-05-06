10c76159795e4654a717ee0471aa67006a5ef8ff
vitrolarium <107374096+vitrolarium@users.noreply.github.com>
2023-05-06 11:33:21 -0300

    clamp player movement and debug buttons

--- Objects/Player/player.gd
+++ Objects/Player/player.gd
@@ -1,7 +1,33 @@
 extends CharacterBody2D
 
+const _SPRITE_SIZE = Vector2(14, 14)
+const _WALKING_AREA = Rect2(0, 0, 112, 192)
+const _CLAMPPED_AREA = Rect2(
+	_WALKING_AREA.position + Vector2(0 + _SPRITE_SIZE.x / 2, _SPRITE_SIZE.y), 
+	_WALKING_AREA.end - Vector2(_SPRITE_SIZE.x, _SPRITE_SIZE.y))
+
 @export var speed : int = 98
 
-func _physics_process(delta: float) -> void:
-	velocity = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down")).normalized() * speed
-	move_and_slide()
+var is_dead = false
+
+func _physics_process(_delta: float) -> void:
+	var direction = Vector2(
+		Input.get_axis("move_left", "move_right"), 
+		Input.get_axis("move_up", "move_down"))
+	
+	if not is_dead:
+		velocity = direction.normalized() * speed
+		move_and_slide()
+		position = position.clamp(_CLAMPPED_AREA.position, _CLAMPPED_AREA.end)
+
+func shock():
+	$AnimationPlayer.play("shock")
+	is_dead = true
+
+func fall():
+	$AnimationPlayer.play("dead")
+	is_dead = true
+
+func reset():
+	$AnimationPlayer.play("walking")
+	is_dead = false
--- Objects/Scenes/Main.gd
+++ Objects/Scenes/Main.gd
@@ -1,11 +1,28 @@
 extends Node2D
 
+var game_is_running = true
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$SidewalkManager.is_enabled = true#pass # Replace with function body.
+	$SidewalkManager.is_enabled = true
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_die_pressed() -> void:
+	if game_is_running:
+		$SidewalkManager.is_enabled = false
+		game_is_running = false
+		$Player.fall()
+	else:
+		game_is_running = true
+		$SidewalkManager.is_enabled = true
+		$Player.reset()
+
+
+func _on_shock_pressed() -> void:
+	if game_is_running:
+		$SidewalkManager.is_enabled = false
+		game_is_running = false
+		$Player.shock()
+	else:
+		game_is_running = true
+		$SidewalkManager.is_enabled = true
+		$Player.reset()
