e31aded448e34fcbcb32eaf537b77c5249fddd32
vitrolarium <107374096+vitrolarium@users.noreply.github.com>
2023-05-08 12:04:21 -0300

    little tweaks on peoples

--- Objects/WalkingPeoples/walking-people-base.gd
+++ Objects/WalkingPeoples/walking-people-base.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
+const PLAYER_COLLISION_LAYER = 2
 const WALK_DIRECTION := Vector2.DOWN
 
 @export var speed : int = 98
+@export var ignore_player : bool = false
 
 var enabled : bool = false
 
@@ -14,13 +16,30 @@ func deactivate():
 	enabled = false
 	$AnimationPlayer.pause()
 
+func walk_dodge(dodge_direction : Vector2):
+	velocity = (WALK_DIRECTION + dodge_direction).normalized() * speed
+
+func walk():
+	velocity = WALK_DIRECTION * speed
+
+func move():
+	if $ObjectDetection.is_colliding():
+		if ignore_player:
+			for i in $ObjectDetection.get_collision_count():
+				var col = $ObjectDetection.get_collider(i) as CollisionObject2D
+				if col and not col.get_collision_layer_value(PLAYER_COLLISION_LAYER):
+					walk_dodge(Vector2.LEFT)
+					break
+		else:
+			walk_dodge(Vector2.RIGHT)
+	else:
+		walk()
+	move_and_slide()
+
+
 func _physics_process(_delta: float) -> void:
 	if enabled:
 		move()
 
-func move():
-	if $ObjectDetection.is_colliding():
-		velocity = (WALK_DIRECTION + Vector2.RIGHT).normalized() * speed
-	else:
-		velocity = WALK_DIRECTION * speed
-	move_and_slide()
+func leave_screen() -> void:
+	self.queue_free()
