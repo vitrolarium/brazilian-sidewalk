40aea7da8c30598d92de61be5225bba5617af981
vitrolarium <107374096+vitrolarium@users.noreply.github.com>
2023-05-09 19:48:57 -0300

    walking people improvements

--- Objects/WalkingPeoples/walking-people-base.gd
+++ Objects/WalkingPeoples/walking-people-base.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
-const PLAYER_COLLISION_LAYER = 2
+const PLAYER_COLLISION_VALUE = 2
 const WALK_DIRECTION := Vector2.DOWN
 
 @export var speed : int = 98
+@export var dodge_speed : int = 49
 @export var ignore_player : bool = false
 
 var enabled : bool = false
@@ -12,26 +13,27 @@ func activate():
 	enabled = true
 	$AnimationPlayer.play()
 
+func is_player_on_detection(collider_index) -> bool:
+	return $ObjectDetection.get_collider(0).get_collision_layer_value(PLAYER_COLLISION_VALUE)
+
 func deactivate():
 	enabled = false
 	$AnimationPlayer.pause()
 
-func walk_dodge(dodge_direction : Vector2):
-	velocity = (WALK_DIRECTION + dodge_direction).normalized() * speed
+func walk_dodge(dodge_direction : float):
+	velocity = WALK_DIRECTION * speed
+	velocity.x = clampf(dodge_direction, -1, 1) * dodge_speed
 
 func walk():
 	velocity = WALK_DIRECTION * speed
 
 func move():
 	if $ObjectDetection.is_colliding():
-		if ignore_player:
-			for i in $ObjectDetection.get_collision_count():
-				var col = $ObjectDetection.get_collider(i) as CollisionObject2D
-				if col and not col.get_collision_layer_value(PLAYER_COLLISION_LAYER):
-					walk_dodge(Vector2.LEFT)
-					break
+		var dodge_dir = position.x - $ObjectDetection.get_collider(0).position.x
+		if (ignore_player and not is_player_on_detection(0)) or (not ignore_player):
+			walk_dodge(dodge_dir)
 		else:
-			walk_dodge(Vector2.RIGHT)
+			walk_dodge(-dodge_dir)
 	else:
 		walk()
 	move_and_slide()
@@ -41,5 +43,5 @@ func _physics_process(_delta: float) -> void:
 	if enabled:
 		move()
 
-func leave_screen() -> void:
+func _leave_screen() -> void:
 	self.queue_free()
