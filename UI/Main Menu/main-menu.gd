4f75fc10f08e015ebaeb8bc313ef8ea629cc1910
vitrolarium <107374096+vitrolarium@users.noreply.github.com>
2023-07-11 14:39:23 -0300

    tiny improvements

--- UI/Main Menu/main-menu.gd
+++ UI/Main Menu/main-menu.gd
@@ -3,7 +3,12 @@ extends Control
 @export var main_game : PackedScene
 
 func _ready() -> void:
-	$Panel/Button.grab_focus()
+	%Play.grab_focus()
+	%Play.connect("pressed", start_game)
+	%Leave.connect("pressed", exit_game)
 
-func _on_button_pressed() -> void:
+func start_game() -> void:
 	get_tree().change_scene_to_packed(main_game)
+
+func exit_game():
+	get_tree().quit()
